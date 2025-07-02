f1f221de16f1c45c20227f7e310fae288c933d92
HemaRamaswamy08 <hemavathibr08@gmail.com>
2025-07-02 15:48:28 +0700

    Immutable class code updated

--- src/task/ImmutableClass.java
+++ src/task/ImmutableClass.java
@@ -4,19 +4,13 @@ public final class ImmutableClass {
 	
 	private final String name;
 	private final int id;
-	private static ImmutableClass instance;
 	
-	private ImmutableClass(String name, int id) {
+	public ImmutableClass(String name, int id) {
 		this.name=name;
 		this.id=id;
 	}
 	
-	 public static ImmutableClass getInstance(String name, int id) {
-	        if (instance == null) {
-	            instance = new ImmutableClass(name, id);
-	        }
-	        return instance;
-	    }
+	
 	
 	public String getName() {
 		return name;
--- src/task/ImmutableMain.java
+++ src/task/ImmutableMain.java
@@ -4,10 +4,9 @@ public class ImmutableMain {
 	
 	public static void main(String[] args) {
 		
-		ImmutableClass obj = ImmutableClass.getInstance("hema",101);
-		obj = ImmutableClass.getInstance("nish", 10);
-		ImmutableClass obj2 = ImmutableClass.getInstance("resh",10);
-		System.out.println(obj2);
+		ImmutableClass obj = new ImmutableClass("hema",101);
+		System.out.println(obj);
+		obj = new ImmutableClass("rrr",444);
 	}
 
 }
