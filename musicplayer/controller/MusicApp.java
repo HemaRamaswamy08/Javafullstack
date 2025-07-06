882c5cad4923c181ee1e94a8697669503c5f5985
HemaRamaswamy08 <hemavathibr08@gmail.com>
2025-07-06 19:29:36 +0700

    code is updated by using Switch case new version

--- musicplayer/controller/MusicApp.java
+++ musicplayer/controller/MusicApp.java
@@ -27,7 +27,7 @@ public class MusicApp {
 				isvalid = true;
 			} catch (InputMismatchException e) {
 				System.err.println("An Error occured : Please enter a proper input");
-				UserInput.SCANNER.next(); 
+				UserInput.SCANNER.next();
 			}
 		} while (!isvalid);
 
@@ -35,13 +35,13 @@ public class MusicApp {
 
 	public static void function(int choice) {
 		switch (choice) {
-		case 1:
+		case 1 -> {
 			MusicPlayer info = songDetails();
 			song.create(info);
 			choice();
-			break;
+		}
 
-		case 2:
+		case 2 -> {
 			int value = UserInput.getUserInputInt("Movie Id");
 			String name = UserInput.getUserInputString(" movie name or song name to update");
 			try {
@@ -52,17 +52,17 @@ public class MusicApp {
 				System.err.println("An error occured :" + e.getMessage());
 			}
 			choice();
-			break;
-		case 3:
+		}
+		case 3 -> {
 			try {
 				song.display();
 			} catch (NoSongsAvailableException e) {
 				System.out.println(e.getMessage());
 			}
 			choice();
-			break;
+		}
 
-		case 4:
+		case 4 -> {
 			int movieId = UserInput.getUserInputInt("Movie Id to delete");
 			try {
 				song.delete(movieId);
@@ -70,19 +70,18 @@ public class MusicApp {
 				System.err.println("An error occured : " + e.getMessage());
 			}
 			choice();
-			break;
-		case 5:
-			name = UserInput.getUserInputString(" Movie name or song name  to search a song");
+		}
+
+		case 5 -> {
 			try {
+				String name = UserInput.getUserInputString(" Movie name or song name  to search a song");
 				song.search(name);
-			}catch (NoSongsAvailableException e) {
+			} catch (NoSongsAvailableException e) {
 				System.out.println("Your List is empty");
 			}
 			choice();
-			break;
-		case 6:
-			System.out.println("THank You for Using");
-			break;
+		}
+		case 6 -> System.out.println("THank You for Using");
 		}
 	}
 
