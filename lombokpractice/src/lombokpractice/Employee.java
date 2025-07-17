d762df6d0c482f526fbb4aac7033fe024e8a1a07
HemaRamaswamy08 <hemavathibr08@gmail.com>
2025-07-17 16:39:12 +0530

    lamboka code

---
+++ lombokpractice/src/lombokpractice/Employee.java
@@ -0,0 +1,13 @@
+package lombokpractice;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class Employee {
+	private int id;
+	private String name;
+
+}
---
+++ lombokpractice/src/lombokpractice/Main.java
@@ -0,0 +1,12 @@
+package lombokpractice;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Employee emp = new Employee();
+		emp.setId(10);
+		emp.setName("Hema");
+		System.out.println(emp.toString());
+	}
+
+}
